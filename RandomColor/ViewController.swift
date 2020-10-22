4b7c56851e81032297c1df243ee25bcd282db65e
Jacob Bathan <jacobbathan@gmail.com>
2020-10-21 20:20:32 -0700

    Initial Commit

---
+++ RandomColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColor
+//
+//  Created by Jacob Bathan on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
